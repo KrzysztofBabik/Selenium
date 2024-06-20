1bec38d3891f6b02c5c8e003f563f284d14c6635
KrzysztofBabik <krzysztofbabik.kb@gmail.com>
2024-06-20 19:10:40 +0200

    Initial commit

---
+++ UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace ExamplesOfSeleniumTesting
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
